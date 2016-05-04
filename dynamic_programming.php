c01c523f66f35e225034c3810413e1007cc11b9a
gyarasu <dev.yoshitsugu@gmail.com>
2016-05-04 14:56:30 +0900

    微修正

--- dynamic_programming.php
+++ dynamic_programming.php
@@ -1,9 +1,11 @@
 <?php
+// https://paiza.jp/poh/kirishima
+
 fscanf(STDIN, "%d", $num_people);
 fscanf(STDIN, "%d", $num_company);
 
 $memo = array();
-$memo[0] = 0;   // 該当の人数をそのまま入れる判定時に別処理にならなくて良いように必要
+$memo[0] = 0;   // 該当の人数をそのまま入れる判定時に別処理にならなくて良いように初期値を準備
 
 for ($i = 0; $i < $num_company; $i++) {
     $memo_tmp = $memo;  // 判定用の配列を用意しておくほうが処理速度が早い（foreachのループ毎に判定対象の配列の大きさが一定）
@@ -13,9 +15,7 @@ for ($i = 0; $i < $num_company; $i++) {
         if (!array_key_exists($key+$people, $memo_tmp)  || $memo_tmp[$key+$people] > $value + $cost) {
             $memo[$key+$people] = $value + $cost;
         }
-
     }
-
 }
 
 asort($memo);
@@ -24,5 +24,5 @@ foreach ($memo as $key => $value) {
         continue;
     }
     echo $value;
-    exit;
+    break;
 }
